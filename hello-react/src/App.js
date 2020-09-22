b9762f6c3b02ffc1a3a445d2a7d543eb09104ca4
zhoupeng <1034935762@qq.com>
2020-09-23 00:05:16 +0800

    脚手架 fix

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg'; // React脚手架中引入图片的方式
+import './App.css'; // React脚手架中引入样式的方式
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <h1 className="demo">这是我第一次使用脚手架</h1>
+    )
+  }
 }
-
-export default App;
